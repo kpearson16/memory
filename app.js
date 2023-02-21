eda19c2414c4f17bc8317e553ac08d76bf8136c0
kpearson16 <122332523+kpearson16@users.noreply.github.com>
2023-02-21 15:24:39 -0500

    first

---
+++ app.js
@@ -0,0 +1,72 @@
+const allCards = [
+    {
+    name: 'amused',
+    URL: 'images/amused.jpeg'
+    },
+    {
+    name: 'baby',
+    URL: 'images/baby.jpeg'
+    },
+    {
+    name: 'beanie',
+    URL: 'images/beaniebaby.jpeg'
+    },
+    {
+    name: 'frog',
+    URL: 'images/Frog-Meme-PNG-Transparent.png'
+    },
+    {
+    name: 'girl',
+    URL: 'images/girl.jpeg'
+   },
+   {
+    name: 'infamous',
+    URL: 'images/infamous.jpeg'
+    },
+    {
+    name: 'jordan',
+    URL: 'images/jordan.jpeg'
+    },
+    {
+    name: 'frog',
+    URL: 'images/Frog-Meme-PNG-Transparent.png'
+    },
+    {
+    name: 'kermit',
+    URL: 'images/kermittea.jpeg'
+    },
+    {
+    name: 'kid2',
+    URL: 'images/kid2.jpeg'
+    },
+    {
+    name: 'laugh',
+    URL: 'images/laugh.png'
+    },
+    {
+    name: 'think',
+    URL: 'images/think.jpeg'
+    },
+    {
+    name: 'what',
+    URL: 'images/whattt.jpeg'
+    },
+    {
+    name: 'worried',
+    URL: 'images/worriedkermit.jpeg'
+    },
+  
+]
+// gameboard
+const board = document.querySelector('board')
+
+
+
+
+
+// flip
+//match
+
+//mismatch
+
+//win 
