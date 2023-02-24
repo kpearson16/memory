d3744947092ab1424bb30136b8119abcf8d238de
kpearson16 <122332523+kpearson16@users.noreply.github.com>
2023-02-24 15:40:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// gameboard
+let counter = 0;
+let firstFlip= '';
+let secondFlip= '';
+// check for match
+const cards = document.querySelectorAll('.cards, .card');
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      card.classList.add('selected'); 
+      if (counter === 0){
+        firstFlip = card.getAttribute('meme');
+        counter++;
+      } else {
+        secondFlip= card.getAttribute('meme');
+        counter = 0;
+
+        if (firstFlip === secondFlip){
+            const match = document.querySelectorAll(
+                ".card[meme='" + firstFlip + "']"
+            );
+            match[0].classList.add('flipped');
+            match[0].classList.remove('clicked');
+            match[1].classList.add('flipped');
+            match[1].classList.remove('selected');
+        } else {
+            const nonMatch = 
+            document.querySelectorAll('.card.selected');
+            setTimeout(() => {
+            nonMatch[0].classList.remove('selected');
+            nonMatch[1].classList.remove('selected');
+        }, 800);
+      }
+}
+let game = document.querySelectorAll('.card');
+game.forEach(function(card){
+  let randomNum = math.floor(math.random() * 24);
+  card.style.order = randomNum;
+})
+    })
+}); 
