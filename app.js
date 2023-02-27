3b926dbbaf570faf027023474de961e261928905
kpearson16 <122332523+kpearson16@users.noreply.github.com>
2023-02-27 13:56:13 -0500

    making cards in js- redoing everything

--- app.js
+++ app.js
@@ -1,64 +1,41 @@
 let counter = 0;
-let firstSelection = "";
-let secondSelection = "";
-
+let firstFlip = "";
+let secondFlip = "";
 const cards = document.querySelectorAll(".cards .card");
 cards.forEach((card) => {
   card.addEventListener("click", () => {
     card.classList.add("selected");
 //check for match
     if (counter === 0) {
-      firstSelection = card.getAttribute("meme");
+      firstFlip = card.getAttribute("meme");
       counter++;
     } else {
-      secondSelection = card.getAttribute("meme");
+      secondFlip = card.getAttribute("meme");
       counter = 0;
 
-      if (firstSelection === secondSelection) {
-        const correctCards = document.querySelectorAll(
+      if (firstFlip === secondFlip) {
+        const matches = document.querySelectorAll(
           ".card[meme='" + firstSelection + "']"
         );
 
-        correctCards[0].classList.add("flipped");
-        correctCards[0].classList.remove("selected");
-        correctCards[1].classList.add("flipped");
-        correctCards[1].classList.remove("selected");
+        matches[0].classList.add("flipped");
+        matches[0].classList.remove("selected");
+        matches[1].classList.add("flipped");
+       matches[1].classList.remove("selected");
       } else {
-        const incorrectCards = document.querySelectorAll(".card.selected");
+        const nonmatches = document.querySelectorAll(".card.selected");
 
        //flip over cards that werent matches
 
         setTimeout(() => {
-          incorrectCards[0].classList.remove("selected");
-          incorrectCards[1].classList.remove("selected");
+          nonmatches[0].classList.remove("selected");
+          nonmatches[1].classList.remove("selected");
         }, 600);
-      }
-      function shuffle() {
-        let cards = document.querySelector(".cards");
-        const btn=document.querySelector('btn');
-        btn.addEventListener("click", function shuffle(){
-          alert("shuffled!");
-        })
+        setTimeout(() => {
+          
+        }, 4500);
 
-        let elementsArray = Array.prototype.slice.call(cards.getElementsByClassName('cards'));
-          elementsArray.forEach(function(element){
-          cards.removeChild(element);
-        })
-        shuffleArray(elementsArray);
-        elementsArray.forEach(function(element){
-        cards.appendChild(element);
-      })
+        }
       }
-      
-      function shuffleArray(array) {
-          for (var i = array.length - 1; i > 0; i--) {
-              var j = Math.floor(Math.random() * (i + 1));
-              var temp = array[i];
-              array[i] = array[j];
-              array[j] = temp;
-          }
-          return array;
-      }
-    }
-  })
+    })
 });
